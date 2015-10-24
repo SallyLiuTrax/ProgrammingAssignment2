d48e5422a1c86b7a0b9e57f7823231482db6f7ba
Sally Liu <venusayaka@gmail.com>
2015-10-24 22:59:59 +0800

    Project Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Below function will construct a new type of Matrix, which has the aiblity to 
+## cache the inverse of the original matrix. 
+## It achieves this by using scoping assignment (<<-) and directly modify variables in the parent environment.
+## Assume the original matrix is always invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function will calculate the inverse of the input matrix.
+## It takes the above defined new type of Matrix and will check whether it has already
+## cached an inverse value. If yes, then it will directly return the inverse without calculating again.
+## If no, it will do perform inverse on the matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
